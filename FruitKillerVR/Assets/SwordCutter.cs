ebac2e11d92d5a806b59c00ab7ebf4c80341eab8
Eric Chen <ericchen@Erics-15-inch-Retina-MBP.local>
2017-02-05 18:13:59 -0800

    init commit

---
+++ FruitKillerVR/Assets/SwordCutter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
+public class SwordCutter : MonoBehaviour {
+
+	public Material capMaterial;
+    public static int int_score;
+    public TextMesh scoreText;
+ 
+ 
+    void Start() {
+        int_score = 1;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+        {
+            pieces[1].AddComponent<Rigidbody>();
+            MeshCollider temp= pieces[1].AddComponent<MeshCollider>();
+            temp.convex = true;
+            //GetComponent<TextMesh>().text = "testing".ToString();//"Score:" + int_score;
+
+            //scoreText = GetComponent<TextMesh>();
+
+        }
+
+        //Destroy(pieces[1], 1);
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score: " + int_score.ToString();//"Score:" + int_score;
+        
+    }
+
+}
